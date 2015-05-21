e31099b7809e6a82545fac5bbcb749c953be70d4
kahfei <bak.kahfei@gmail.com>
2015-05-21 11:01:29 +0800

    import csv working

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -74,6 +74,6 @@ class MaterialsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def material_params
-      params.require(:material).permit(:number, :description, :plant_status, :type, :procurement_type, :eis_start_date, :setup_costing_date, :setup_purchasing_date, :engineering_release_date, :finalized_purchasing_date, :clp_prl_date, :clp_sdl_date, :work_scheduling, :plant_costing_date, :quality_management_date)
+      params.require(:material).permit(:number, :description, :plant_status, :material_type, :procurement_type, :eis_start_date, :setup_costing_date, :setup_purchasing_date, :engineering_release_date, :finalized_purchasing_date, :clp_prl_date, :clp_sdl_date, :work_scheduling, :plant_costing_date, :quality_management_date)
     end
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,5 +1,4 @@
 class Material < ActiveRecord::Base
-  require 'csv'
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
