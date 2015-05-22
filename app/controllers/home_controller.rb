92956affafc955f05793925f74fa80b0779bfc81
kahfei <bak.kahfei@gmail.com>
2015-05-22 09:03:32 +0800

    complete average report for 2014 and 2015

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -60,7 +60,61 @@ class HomeController < ApplicationController
 
     
 
+# 2015 reports
+    # semifinished + make + pre
+    @semifinished_make_pre_materials_2015 = Material.where("engineering_release_date IS NOT Null").where(
+                                      "material_type = ? AND procurement_type = ? ",
+                                      "Semifinished products", "E").where("strftime('%Y', eis_start_date)     = ?", "2015")
+    @smp_length_2015 = @semifinished_make_pre_materials_2015.to_a.length
+    @smp_setup_costing_average_2015 = @semifinished_make_pre_materials_2015.to_a.sum(&:setup_costing_duration).to_f / @smp_length_2015
+    @smp_engineering_release_average_2015 =  @semifinished_make_pre_materials_2015.to_a.sum(&:engineering_release_duration).to_f / @smp_length_2015
+    @smp_log_prod_average_2015 = @semifinished_make_pre_materials_2015.to_a.sum(&:setup_log_prod_duration).to_f / @smp_length_2015
+    @smp_plant_costing_average_2015 = @semifinished_make_pre_materials_2015.to_a.sum(&:plant_costing_duration).to_f / @smp_length_2015
+    @smp_quality_management_average_2015 = @semifinished_make_pre_materials_2015.to_a.sum(&:quality_management_duration).to_f / @smp_length_2015
 
+    # semifinished + make + normal
+    @semifinished_make_normal_materials_2015 = Material.where("engineering_release_date IS Null").where(
+                                      "material_type = ? AND procurement_type = ? ",
+                                      "Semifinished products", "E").where("strftime('%Y', eis_start_date)     = ?", "2015")
+    @smn_length_2015 = @semifinished_make_normal_materials_2015.to_a.length
+    @smn_setup_costing_average_2015 = @semifinished_make_normal_materials_2015.to_a.sum(&:setup_costing_duration).to_f / @smn_length_2015
+    @smn_log_prod_average_2015 = @semifinished_make_normal_materials_2015.to_a.sum(&:setup_log_prod_duration).to_f / @smn_length_2015
+    @smn_plant_costing_average_2015 = @semifinished_make_normal_materials_2015.to_a.sum(&:plant_costing_duration).to_f / @smn_length_2015
+    @smn_quality_management_average_2015 = @semifinished_make_normal_materials_2015.to_a.sum(&:quality_management_duration).to_f / @smn_length_2015
+
+    # semifinished + buy + normal
+    @semifinished_buy_normal_materials_2015 = Material.where("engineering_release_date IS Null").where(
+                                      "material_type = ? AND procurement_type = ? ",
+                                      "Semifinished products", "F").where("strftime('%Y', eis_start_date)     = ?", "2015")
+    @sbn_length_2015 = @semifinished_buy_normal_materials_2015.to_a.length
+    @sbn_setup_costing_average_2015 = @semifinished_buy_normal_materials_2015.to_a.sum(&:setup_costing_duration).to_f / @sbn_length_2015
+    @sbn_setup_purchasing_average_2015 = @semifinished_buy_normal_materials_2015.to_a.sum(&:setup_purchasing_duration).to_f / @sbn_length_2015
+    @sbn_log_prod_average_2015 = @semifinished_buy_normal_materials_2015.to_a.sum(&:setup_log_prod_duration).to_f / @sbn_length_2015
+    @sbn_plant_costing_average_2015 = @semifinished_buy_normal_materials_2015.to_a.sum(&:plant_costing_duration).to_f / @sbn_length_2015
+    @sbn_quality_management_average_2015 = @semifinished_buy_normal_materials_2015.to_a.sum(&:quality_management_duration).to_f / @sbn_length_2015
+
+    # semifinished + buy + Pre-series
+    @semifinished_buy_pre_materials_2015 = Material.where("engineering_release_date IS NOT Null").where(
+                                      "material_type = ? AND procurement_type = ? ",
+                                      "Semifinished products", "F").where("strftime('%Y', eis_start_date)     = ?", "2015")
+    @sbp_length_2015 = @semifinished_buy_pre_materials_2015.to_a.length
+    @sbp_setup_costing_average_2015 = @semifinished_buy_pre_materials_2015.to_a.sum(&:setup_costing_duration).to_f / @sbp_length_2015
+    @sbp_setup_purchasing_average_2015 = @semifinished_buy_pre_materials_2015.to_a.sum(&:setup_purchasing_duration).to_f / @sbp_length_2015
+    @sbp_engineering_release_average_2015 =  @semifinished_buy_pre_materials_2015.to_a.sum(&:engineering_release_duration).to_f / @sbp_length_2015
+    @sbp_finalize_purchasing_average_2015 =  @semifinished_buy_pre_materials_2015.to_a.sum(&:finalized_purchasing_duration).to_f / @sbp_length_2015
+    @sbp_log_prod_average_2015 = @semifinished_buy_pre_materials_2015.to_a.sum(&:setup_log_prod_duration).to_f / @sbp_length_2015
+    @sbp_plant_costing_average_2015 = @semifinished_buy_pre_materials_2015.to_a.sum(&:plant_costing_duration).to_f / @sbp_length_2015
+    @sbp_quality_management_average_2015 = @semifinished_buy_pre_materials_2015.to_a.sum(&:quality_management_duration).to_f / @sbp_length_2015
+
+    # finished + make
+    @finished_make_materials_2015 = Material.where(
+                                      "material_type = ? AND procurement_type = ? ",
+                                      "Finished products", "E").where("strftime('%Y', eis_start_date)     = ?", "2015")
+    @fm_length_2015 = @finished_make_materials_2015.to_a.length
+    @fm_setup_costing_average_2015 = @finished_make_materials_2015.to_a.sum(&:setup_costing_duration).to_f / @fm_length_2015
+    @fm_log_prod_average_2015 = @finished_make_materials_2015.to_a.sum(&:setup_log_prod_duration).to_f / @fm_length_2015
+    @fm_plant_costing_average_2015 = @finished_make_materials_2015.to_a.sum(&:plant_costing_duration).to_f / @fm_length_2015
+    @fm_quality_management_average_2015 = @finished_make_materials_2015.to_a.sum(&:quality_management_duration).to_f / @fm_length_2015
 
   end
 end
